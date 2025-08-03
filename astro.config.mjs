c3a7698a370462335e098c7a582dd103c352c3ab
9h8x <76052920+9h8x@users.noreply.github.com>
2025-08-02 23:53:54 -0300

    added cloudflare, sitemap, seo and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
- import icon from "astro-icon";
+import icon from "astro-icon";
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare(),
+  output: 'static'
 });
